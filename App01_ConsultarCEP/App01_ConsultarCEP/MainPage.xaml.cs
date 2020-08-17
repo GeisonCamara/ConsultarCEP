ba5d08d9e5e4c514c7cc80d44fdfb377d470dc59
geison.camara <geison.camara@digitaldesk.com.br>
2020-08-17 17:22:19 -0300

    Git Init Implementação dos métodos de consulta ao WebService ViaCEP Implementação da Consulta ao WebService ViaCEP e retorno dos dados

---
+++ App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using App01_ConsultarCEP.Servico;
+using App01_ConsultarCEP.Servico.Modelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnBuscarCep.Clicked += BuscarCep;
+        }
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            string cep = txtCep.Text.Trim();
+            Endereco endereco = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+            lblResult.Text = string.Format("Endereço: {0}, {1} {2}", endereco.Localidade, endereco.Uf, endereco.Logradouro);
+        }
+    }
+}
---
+++ App01_ConsultarCEP/App01_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,24 @@
+﻿using App01_ConsultarCEP.Servico.Modelo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace App01_ConsultarCEP.Servico
+{
+    class ViaCEPServico
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string novaConsultaURL = string.Format(EnderecoURL, cep);
+
+            WebClient client = new WebClient();
+            string clientResponse = client.DownloadString(novaConsultaURL);
+
+            return JsonConvert.DeserializeObject<Endereco>(clientResponse);
+        }
+    }
+}
